e886df5223d9d1ab4ef3497a0d0eecf7071b9d59
April Barrett <abarrett@treetopllc.com>
2014-12-31 11:31:38 -0800

    add asset types

--- index.js
+++ index.js
@@ -127,6 +127,34 @@ exports.activity_types = index([
     "Delete"
 ]);
 
+exports.asset_types = index([
+    0: "notype",
+    1: "aif",
+    2: "doc",
+    3: "docx",
+    4: "gif",
+    5: "jpg",
+    6: "mov",
+    7: "mp3",
+    8: "mpg",
+    9: "odp",
+    10: "odt",
+    11: "oga",
+    12: "ogf",
+    13: "pdf",
+    14: "png",
+    15: "pps",
+    16: "ppsx",
+    17: "ppt",
+    18: "pptx",
+    19: "txt",
+    20: "wma",
+    21: "wmv",
+    22: "xls",
+    23: "xlsx",
+    24: "rtf"
+]);
+
 function index(list) {
     var by_name = {};
     var by_id = {};
